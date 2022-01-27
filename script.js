6e8278e37ea4b5f8069b9a0c7cf0e5a779417f24
Abhay1kumar <kumarabhay401@gmail.com>
2022-01-27 21:52:10 +0530

    push and add

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-document.querySelector(".btn").addEventListener("click",function(){
-    let text=document.querySelector('.form-control').value;
-    console.log(text);
-    let newlist=document.createElement("li");
+let labor=[
+    {
+    name: 'labor1',
+    payment:100
+},
+{
+    name: 'labor2',
+    payment:150
+},
+{
+    name: 'labor3',
+    payment:250
+}
+]
 
-    newlist.innerHTML=`<p><input type="checkbox" class="checkbox">${text}</p>`;
-    document.querySelector(".list").appendChild(newlist);
-});
+
+//add item in the array at last position
+// labor.push({
+//     name: 'labor4',
+//     payment:200
+// })
+
+// add item in the array at first position
+// labor.unshift({
+//     name:"labor-head",
+//     payment:300
+// })
+
+// delete item in the array at last position
+// labor.pop()
+
+
+// delete item in the array at first position
+// labor.shift()
+
+console.log(labor);
